d52c4b08a5ed28364b41a5ba94043a04a690fdc4
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-03 19:20:43 +0300

    Netlify setup

--- react-frontend/src/utils.ts
+++ react-frontend/src/utils.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8000"; 
-export const BACKEND_URL = "https://ec2-16-16-92-151.eu-north-1.compute.amazonaws.com"
+export const BACKEND_URL = "http://13.49.138.7"
