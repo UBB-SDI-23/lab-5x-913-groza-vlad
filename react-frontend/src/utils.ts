02c0638b5e524fac1c352b5e1e4da9c0bcffd023
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-03 18:39:58 +0300

    Update utils.ts

--- react-frontend/src/utils.ts
+++ react-frontend/src/utils.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8000"; 
-export const BACKEND_URL = "http://13.49.134.211/<path-to-access-cors-url>"
+export const BACKEND_URL = "https://ec2-16-16-92-151.eu-north-1.compute.amazonaws.com"
