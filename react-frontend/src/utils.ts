04689df3666e7032f95459b6bafdd3ffc4e5190e
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-06 18:16:17 +0300

    Update utils.ts

--- react-frontend/src/utils.ts
+++ react-frontend/src/utils.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "http://localhost:8000"; 
-export const BACKEND_URL = "http://13.48.134.64"
+// export const BACKEND_URL = "http://localhost:8000/api"; 
+export const BACKEND_URL = "https://sdi-api.jumpingcrab.com/api"
