411b165b9e6bc26eda8cf02a4de7a32dac4b4911
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-06 14:21:25 +0300

    Update url

--- application/application/urls.py
+++ application/application/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('', include('players.urls'))
+    path('api/', include('players.urls'))
 ]
--- react-frontend/src/utils.ts
+++ react-frontend/src/utils.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8000"; 
-export const BACKEND_URL = "http://13.49.138.7"
+export const BACKEND_URL = "http://13.48.134.64"
