78534acd7e3d7b501f43d3cc43fcfe064d0b5891
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-22 19:12:19 +0300

    Update commit

--- application/db_data/populate.sql
+++ application/db_data/populate.sql
@@ -5,6 +5,7 @@ ALTER TABLE players_record DROP CONSTRAINT players_record_club_id_34ed57d6_fk_pl
 ALTER TABLE players_record DROP CONSTRAINT players_record_competition_id_f0e666e7_fk_players_c;
 ALTER TABLE players_footballplayer DROP CONSTRAINT players_footballplay_club_id_c0f157b9_fk_players_f;
 
+GO
 
 -- delete data from tables
 TRUNCATE TABLE players_footballclub;
@@ -18,7 +19,7 @@ GO
 COPY players_footballclub FROM 'football_clubs.csv' CSV HEADER;
 COPY players_footballplayer FROM 'players.csv' CSV HEADER;
 COPY players_competition FROM 'competitions.csv' CSV HEADER;
-COPY players_record FROM 'records.csv' CSV HEADER;
+-- COPY players_record FROM 'records.csv' CSV HEADER;
 
 GO
 
