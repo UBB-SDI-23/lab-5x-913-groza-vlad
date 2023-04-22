97acf20f38e8833401bc9405584f3535ac1dcb28
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-22 16:20:49 +0300

    Update commit

--- application/db_data/populate.sql
+++ application/db_data/populate.sql
@@ -1,5 +1,11 @@
 
 
+-- drop constraints
+ALTER TABLE players_record DROP CONSTRAINT players_record_club_id_34ed57d6_fk_players_footballclub_id;
+ALTER TABLE players_record DROP CONSTRAINT players_record_competition_id_f0e666e7_fk_players_c;
+ALTER TABLE players_footballplayer DROP CONSTRAINT players_footballplay_club_id_c0f157b9_fk_players_f;
+
+
 -- delete data from tables
 TRUNCATE TABLE players_footballclub;
 TRUNCATE TABLE players_footballplayer;
@@ -16,3 +22,9 @@ COPY players_record FROM 'records.csv' CSV HEADER;
 
 GO
 
+-- add constraints
+ALTER TABLE players_footballplayer ADD CONSTRAINT players_footballplay_club_id_c0f157b9_fk_players_f FOREIGN KEY (club_id) REFERENCES players_footballclub(id);
+ALTER TABLE players_record ADD CONSTRAINT players_record_club_id_34ed57d6_fk_players_footballclub_id FOREIGN KEY (club_id) REFERENCES players_footballclub(id),
+ALTER TABLE players_record ADD CONSTRAINT players_record_competition_id_f0e666e7_fk_players_c FOREIGN KEY (competition_id) REFERENCES players_competition(id);
+
+GO
