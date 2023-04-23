2ab4ab4c6406b43d745ce366fe01a5eabdd313de
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-04-23 17:18:46 +0300

    Update views.py

--- application/db_data/populate.sql
+++ application/db_data/populate.sql
@@ -12,12 +12,12 @@ TRUNCATE TABLE players_competition;
 TRUNCATE TABLE players_record;
 
 -- insert data from the CSV files into tables
-COPY players_footballclub FROM 'football_clubs.csv' CSV HEADER;
-COPY players_footballplayer FROM 'players.csv' CSV HEADER;
-COPY players_competition FROM 'competitions.csv' CSV HEADER;
-COPY players_record FROM 'records.csv' CSV HEADER;
+\copy players_footballclub FROM 'football_clubs.csv' CSV HEADER;
+\copy players_footballplayer FROM 'players.csv' CSV HEADER;
+\copy players_competition FROM 'competitions.csv' CSV HEADER;
+\copy players_record FROM 'records.csv' CSV HEADER;
 
 -- add constraints
 ALTER TABLE players_footballplayer ADD CONSTRAINT players_footballplay_club_id_c0f157b9_fk_players_f FOREIGN KEY (club_id) REFERENCES players_footballclub(id);
-ALTER TABLE players_record ADD CONSTRAINT players_record_club_id_34ed57d6_fk_players_footballclub_id FOREIGN KEY (club_id) REFERENCES players_footballclub(id),
 ALTER TABLE players_record ADD CONSTRAINT players_record_competition_id_f0e666e7_fk_players_c FOREIGN KEY (competition_id) REFERENCES players_competition(id);
+ALTER TABLE players_record ADD CONSTRAINT players_record_club_id_34ed57d6_fk_players_footballclub_id FOREIGN KEY (club_id) REFERENCES players_footballclub(id);
